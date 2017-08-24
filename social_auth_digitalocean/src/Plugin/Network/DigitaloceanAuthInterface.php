ff016a3d27a6afdd90179a154742cfe05b258f5e
Himanshu Dixit <hudixt@gmail.com>
2017-08-24 12:37:50 +0530

    Initial Commit [Using Theleague library]

---
+++ social_auth_digitalocean/src/Plugin/Network/DigitaloceanAuthInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\social_auth_digitalocean\Plugin\Network;
+
+use Drupal\social_api\Plugin\NetworkInterface;
+
+/**
+ * Defines the Digitalocean Auth interface.
+ */
+interface DigitaloceanAuthInterface extends NetworkInterface {}
