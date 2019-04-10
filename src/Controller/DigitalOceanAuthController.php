a304afd6740bc6be574214f2d6097da10a8381cf
Getulio Sánchez <valentin2507@gmail.com>
2019-04-09 23:20:39 -0400

    Issue #3047027: Pass renderer service to controller constructor

--- src/Controller/DigitalOceanAuthController.php
+++ src/Controller/DigitalOceanAuthController.php
@@ -3,6 +3,7 @@
 namespace Drupal\social_auth_digitalocean\Controller;
 
 use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\Render\RendererInterface;
 use Drupal\social_api\Plugin\NetworkManager;
 use Drupal\social_auth\Controller\OAuth2ControllerBase;
 use Drupal\social_auth\SocialAuthDataHandler;
@@ -31,15 +32,21 @@ class DigitalOceanAuthController extends OAuth2ControllerBase {
    *   Used to access GET parameters.
    * @param \Drupal\social_auth\SocialAuthDataHandler $data_handler
    *   The Social Auth data handler.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   Used to handle metadata for redirection to authentication URL.
    */
   public function __construct(MessengerInterface $messenger,
                               NetworkManager $network_manager,
                               UserAuthenticator $user_authenticator,
                               DigitalOceanAuthManager $digitalocean_manager,
                               RequestStack $request,
-                              SocialAuthDataHandler $data_handler) {
+                              SocialAuthDataHandler $data_handler,
+                              RendererInterface $renderer) {
 
-    parent::__construct('Social Auth DigitalOcean', 'social_auth_digitalocean', $messenger, $network_manager, $user_authenticator, $digitalocean_manager, $request, $data_handler);
+    parent::__construct('Social Auth DigitalOcean', 'social_auth_digitalocean',
+                        $messenger, $network_manager, $user_authenticator,
+                        $digitalocean_manager, $request, $data_handler,
+                        $renderer);
   }
 
   /**
@@ -52,7 +59,8 @@ class DigitalOceanAuthController extends OAuth2ControllerBase {
       $container->get('social_auth.user_authenticator'),
       $container->get('social_auth_digitalocean.manager'),
       $container->get('request_stack'),
-      $container->get('social_auth.data_handler')
+      $container->get('social_auth.data_handler'),
+      $container->get('renderer')
     );
   }
 
@@ -79,7 +87,12 @@ class DigitalOceanAuthController extends OAuth2ControllerBase {
       // Gets (or not) extra initial data.
       $data = $this->userAuthenticator->checkProviderIsAssociated($profile->getId()) ? NULL : $this->providerManager->getExtraDetails();
 
-      return $this->userAuthenticator->authenticateUser($profile->getId(), $profile->getEmail(), $profile->getId(), $this->providerManager->getAccessToken(), FALSE, $data);
+      return $this->userAuthenticator->authenticateUser($profile->getId(),
+                                                        $profile->getEmail(),
+                                                        $profile->getId(),
+                                                        $this->providerManager->getAccessToken(),
+                                             FALSE,
+                                                        $data);
     }
 
     return $this->redirect('user.login');
